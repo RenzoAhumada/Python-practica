25c4ddd5db78b3e909a9c87df4ffe93b90ac0fa5
RenzoAhumada <renzo.ahumada21@gmail.com>
2022-06-16 23:56:02 -0300

    Se termina proyecto final

--- Bicicleteria/Bicicleteria.py
+++ Bicicleteria/Bicicleteria.py
@@ -1,36 +1,31 @@
 
 
+#Creamos una clase bicicleteria, la cual va a tener sus atributos y metodos
+class bicicleteria:
+    #Aqui igualamos todo a 0 para que el conteo empiece desde ese y no empiece con otro numero al azar
+    bicicletas = 0
+    bicicletas = bicicletas + 1
+    cantidad_de_ventas = int
+    
+    def vender_bicicleta():
+        cantidad_vendida = cantidad_vendida + 1
+        #Subimos la cantidad de de bicicletas vendidas
 
+    def comprar_bicicleta():
+        cantidad_comprada = cantidad_comprada + 1
+        #Desde aqui subimos la cantidad de bicicletas compradas
 
-
-
-class bicicleteria():
-    def __init__(self) -> None:
-        bicicletas = {}
-        self.bicicletas = bicicletas [Bicicleta]
-        ganancias = float(0,0)
-        cantidad_de_ventas = int(0)
-        self.ganancias = ganancias
-        self.cantidad_de_ventas = cantidad_de_ventas
+    def gananciasDeclaradas(self):
+        declarar_ganancias = input(float("Ingrese ganancias de las ventas: "))
+        self.declarar_ganancias = declarar_ganancias
+        print(f"Total ganado es de: ", {self.declarar_ganancias})
+        #Desde aqui podemos declarar las ganancias que se tiene con la venta de una bicicleta
         
 
-    def vender_bicicleta(self, Bicicleta):
-        venta_bici = self.cantidad_de_ventas + 1
-        self.cantidad_de_ventas = venta_bici
-        gananciastotal = self.ganancias + self.bicicletas(bici.precio)
-
-        print(venta_bici)
-
-    def comprar_bicicleta(self, Bicicleta):
-        compra_bici = compra_bici + 1
-        
-        
-
-
-
-
-class Bicicleta():
-    def __init__(self) -> None:
+#Instanciamos el clase Bicicleta con sus atributos y metodos
+class Bicicleta:
+    def cargarbici(self) -> None:
+        #Con este metodo cargamos una bicicleta nueva y la instanciamos
         nmero_de_serie =str(input("Numero de serie: "))
         modelo =str(input("Modelo: "))
         anio = int(input("Año: "))
@@ -40,7 +35,8 @@ class Bicicleta():
         self.anio = anio
         self.precio = precio
 
-    def set_precio(self):
+    def set_precio(self, bici):
+        #Con este metodo podemos modificar el precio de la bicicleta a llamar
         variable = str(input("Ingrese numero de serie de la bicicleta: "))
         if variable == self.nmero_de_serie:
             precio = float(input("Ingrese el nuevo precio de la bicicleta: "))
@@ -49,19 +45,43 @@ class Bicicleta():
             print("Error, el numero de serie no existe")    
 
     def get_precio(self):
+        #Con este metodo obenemos el el precio de la bicicleta
+        #La igualamos a una variable vacia para poder llamar esa bicicleta y que no llame otra
         variable = str(input("Ingrese numero de serie de la bicicleta: "))
         if variable == self.nmero_de_serie:
            print("El precio de la bicicleta es de: ", self.precio)
         elif variable != self.nmero_de_serie:
             print("Error, el numero de serie no existe") 
     def get_nmero_de_serie(self):
+        #Aqui llamamos los numeros de serie de las bicicletas
         print(self.nmero_de_serie)
-
-
+bici = {}
 bici = Bicicleta()
-bici.get_precio()
-bici.get_nmero_de_serie()
-bici.set_precio()
-bici.get_precio()
 bicicleteria1 = bicicleteria()
-print(bicicleteria1)
+opcion = 0
+
+#Un pequeño menu interactivo para poder entrar en los diferentes metodos y poder salir y entrar
+while opcion != 8:
+    print("Opcion 1 crear una bicicleta")
+    print("Opcion 2 cambiar el precio de una bicicleta")
+    print("Opcion 3 ver el precio de una bicicleta")
+    print("Opcion 4 ver el numeros de serie de una bicicleta")
+    print("Opcion 5 ver cantidad de bicicletas compradas")
+    print("Opcion 6 ver cantidad de bicicletas vendidas")
+    opcion = int(input("Ingrese la opcion que desea: "))
+    if opcion == 1:
+        bici.cargarbici()
+        bicicleteria1()
+        bicicleteria1.comprar_bicicleta()
+    if opcion == 2:
+        bici.set_precio()
+    if opcion == 3:
+        bici.get_precio()
+    if opcion == 4:
+        bici.get_nmero_de_serie()
+    if opcion == 5:
+        print(bicicleteria1.comprar_bicicleta())
+    if opcion == 6:
+        print(bicicleteria1.vender_bicicleta()) 
+    if opcion == 8:
+        break
--- superheroesjson/heroes.py
+++ superheroesjson/heroes.py
@@ -1,10 +1,12 @@
+
+#Importamos json
 import json
 
-
+#Creamos una nueva lista vacia para poder appendiar los datos
 heroes = {}
-
+#Le damos la llave hereos a la lista de hereos para poder appendiar los difrentes tipos de hereos
 heroes["hereos"] = []
-
+#Se appendean los datos
 heroes["hereos"].append({"nombre": "Flash",
                         "identidadSecreta": "Bartholomew Henry 'Barry' Allen",
                         "poderes": [
@@ -28,5 +30,5 @@ heroes["hereos"].append({ "nombre": "Super Man",
                         "durabilidad", 
                         "sentidos y longevidad"
                         ]})
-
+#Imprimimos por pantalla la lista que tiene el json guardad.
 print(heroes)
